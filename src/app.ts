32a6d0da002a20b4f1247975f1015abdc2d78a93
JohnnyOak85 <joao.cmp@proton.me>
2023-03-30 23:36:19 +0100

    Refactored app/server

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,8 @@
-import { notFound } from '@hapi/boom';
-import { server } from '@hapi/hapi';
-import { config } from 'dotenv';
-import { readFileSync } from 'fs-extra';
-import hapiAuthJwt2 from 'hapi-auth-jwt2';
-import { validate } from './helpers';
-import { Routes } from './routes';
+import { init } from './helpers';
 
-const init = async () => {
-    try {
-        const { error } = config({ path: `${__dirname}/.env` });
-
-        if (error) {
-            throw notFound('Environment file missing.');
-        }
-
-        const client = server({
-            host: process.env.HOST!,
-            port: process.env.PORT!,
-            routes: {
-                cors: {
-                    origin: ['*'],
-                    headers: ['Accept', 'Content-Type'],
-                    additionalHeaders: ['X-Requested-With']
-                }
-            }
-            // tls: {
-            //     key: readFileSync(`${__dirname}/certs/sword-key.pem`),
-            //     cert: readFileSync(`${__dirname}/certs/sword-cert.pem`)
-            // }
-        });
-
-        await client.register(hapiAuthJwt2);
-
-        client.auth.strategy('jwt', 'jwt', {
-            key: process.env.JWT_KEY!,
-            validate,
-            verifyOptions: { algorithms: ['HS256'] }
-        });
-
-        client.route(Routes);
-
-        await client.start();
-
-        console.log(`Server running on ${client.info.uri}`);
-    } catch (error) {
-        console.log(error);
-    }
-};
+init();
 
 process.on('unhandledRejection', error => {
     console.log('SYSTEM ERROR:', error);
     process.exit(1);
 });
-
-init();
---
+++ src/helpers/server.helper.ts
@@ -0,0 +1,68 @@
+import { notFound } from '@hapi/boom';
+import { server } from '@hapi/hapi';
+import { config } from 'dotenv';
+import hapiAuthJwt2 from 'hapi-auth-jwt2';
+import { Sequelize } from 'sequelize';
+import { Routes } from '../routes';
+import { initClasses } from './database.helper';
+import { validate } from './validation.helper';
+
+export const init = async () => {
+    try {
+        const { error } = config({ path: `${__dirname}/../../.env` });
+
+        if (error) {
+            throw notFound('Environment file missing.');
+        }
+
+        const sequelize = new Sequelize({
+            database: 'sql7607881',
+            dialect: 'mysql',
+            host: 'sql7.freesqldatabase.com',
+            password: 'gyzTpPG4dc',
+            username: 'sql7607881'
+        });
+
+        // const sequelize = new Sequelize({
+        //     database: process.env.DB_NAME!,
+        //     dialect: 'mysql',
+        //     host: process.env.DB_HOST!,
+        //     password: process.env.DB_PASS!,
+        //     port: Number(process.env.DB_PORT!),
+        //     username: process.env.DB_USER!
+        // });
+
+        initClasses(sequelize);
+
+        const client = server({
+            host: process.env.HOST!,
+            port: process.env.PORT!,
+            routes: {
+                cors: {
+                    origin: ['*'],
+                    headers: ['Accept', 'Content-Type'],
+                    additionalHeaders: ['X-Requested-With']
+                }
+            }
+        });
+
+        await client.register(hapiAuthJwt2);
+
+        client.auth.strategy('jwt', 'jwt', {
+            key: process.env.JWT_KEY!,
+            validate,
+            verifyOptions: { algorithms: ['HS256'] }
+        });
+
+        client.route(Routes);
+
+        await client.start();
+
+        console.log(`Server running on ${client.info.uri}`);
+
+        return client;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
