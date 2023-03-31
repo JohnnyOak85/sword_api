eb4f10d4b0f14754954297a74a5874867ea0aec6
JohnnyOak85 <joao.cmp@proton.me>
2023-03-31 19:06:55 +0100

    Cleaned schema

--- src/schemas/Credentials.schema.ts
+++ src/schemas/Credentials.schema.ts
@@ -1,5 +1,5 @@
 import Joi from 'joi';
-import {} from '@hapi/boom';
+
 export const CredentialsSchema = Joi.object().keys({
     password: Joi.string().required().description('Password of the user trying to login.'),
     username: Joi.string().required().description('Identification of the user trying to login.')
